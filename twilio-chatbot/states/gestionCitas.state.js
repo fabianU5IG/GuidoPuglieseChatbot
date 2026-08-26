5a394e2fa2c91f01cac2509e4fbe85448f2fbf76
Sara Mendoza <you@example.com>
2026-08-26 11:46:30 -0500

    fix: arreglo msj plantillas

--- twilio-chatbot/states/gestionCitas.state.js
+++ twilio-chatbot/states/gestionCitas.state.js
@@ -4,7 +4,7 @@ const TEMPLATE_ASK_DOC_TYPE =
     process.env.TWILIO_TEMPLATE_SUPPORT_DOC_TYPE_SID ||
     process.env.TWILIO_TEMPLATE_ASK_DOC_TYPE_SID ||
     "HX3b07c0984e3fc8c6d2f96630752ef101";
-const TEMPLATE_AGENDAMIENTO_INICIO = "HX1d4e991f32d11da12739d2d835110a60";
+const TEMPLATE_AGENDAMIENTO_INICIO = "HXc9f2cfd70960b4b856787a720e2e9a9b";
 
 function sendTemplate(contentSid, nextState, data = {}, variables = null) {
     return {
